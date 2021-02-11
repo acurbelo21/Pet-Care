74e898ca0c34d789da3d8798fde382741eb3c048
SUNTheBaer <padron1999@yahoo.com>
2021-02-10 20:33:20 -0500

    firebase configs and project id

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -3,12 +3,13 @@ import * as firebase from "firebase";
 import "firebase/firestore";
 
 const config = {
-    apiKey: "AIzaSyD-LiSTZojaA1w3Wdciwf7HI8AP6GE7zLA",
-    authDomain: "react-native-fiber.firebaseapp.com",
-    databaseURL: "https://react-native-fiber.firebaseio.com",
-    projectId: "react-native-fiber",
-    storageBucket: "react-native-fiber.appspot.com",
-    messagingSenderId: "387191574162"
+    apiKey: "AIzaSyCE3r7Q2qAWXb1sS483sG-lWew6-EEx6pI",
+    authDomain: "pet-care-b43dd.firebaseapp.com",
+    projectId: "pet-care-b43dd",
+    storageBucket: "pet-care-b43dd.appspot.com",
+    messagingSenderId: "729760486188",
+    appId: "1:729760486188:web:d0a0705a3d6aa63cccd289",
+    measurementId: "G-9P2VHBCKJX"
 };
 
 export default class Firebase {
