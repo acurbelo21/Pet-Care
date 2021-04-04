bcfe946d3a4049c5858e04180b7aa754ec948d00
Cheska <csy003@fiu.edu>
2021-04-04 14:05:23 -0400

    Add PDF Viewing and ViewPager

--- src/home/profile/ViewDocuments.js
+++ src/home/profile/ViewDocuments.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, SafeAreaView } from 'react-native';
+import { StyleSheet, View, SafeAreaView, Dimensions } from 'react-native';
 import Firebase from "../../components/Firebase";
 import { Text, NavHeader, Theme, Button } from "../../components";
 import { LinearGradient } from "expo-linear-gradient";
 import { NavHeaderWithButton } from '../../components';
 import * as DocumentPicker from 'expo-document-picker';
+import PDFReader from 'rn-pdf-reader-js';
+import ViewPager from '@react-native-community/viewpager';
 
 export default class ViewDocuments extends Component {
     async componentDidMount(): Promise<void> {
@@ -119,12 +121,50 @@ export default class ViewDocuments extends Component {
         const { navigation } = this.props;
 
         return (
-            <View>
-                <NavHeaderWithButton title="Documents" back {...{ navigation }} buttonFn={this.chooseFile} buttonIcon="plus" />
-                <SafeAreaView>
-                    <Text>View documents</Text>
-                </SafeAreaView>
-            </View>
+            // <SafeAreaView
+            // style={{
+            //     width: Dimensions.get ('window'). width,
+            //     height: Dimensions.get ('window'). height, backgroundColor:'#000'}}>
+                // <NavHeaderWithButton title="Documents" back {...{ navigation }} buttonFn={this.chooseFile} buttonIcon="plus" />
+            //     {/* <SafeAreaView> */}
+                // <PDFReader
+                //         source={{
+                //         uri: 'https://drive.google.com/file/d/1Aozi9jTceIhrlJuzKVLuKjHhWiChc0dH/view?usp=sharing',
+                //         }}
+                //     />
+            //     {/* </SafeAreaView> */}
+            // </SafeAreaView>
+            <View style={{ flex: 1 }}>
+            <NavHeaderWithButton title="Documents" back {...{ navigation }} buttonFn={this.chooseFile} buttonIcon="plus" />
+            <ViewPager style={styles.viewPager} initialPage={0}>
+              <View style={{
+                width: Dimensions.get ('window'). width,
+                height: Dimensions.get ('window'). height, backgroundColor:'#000'}} key="1">
+                <Text style={{backgroundColor: "white"}}>Please swipe left to view the next PDF.</Text>
+              <PDFReader
+                        source={{
+                        uri: 'https://drive.google.com/file/d/1Aozi9jTceIhrlJuzKVLuKjHhWiChc0dH/view?usp=sharing',
+                        }}
+                    />
+              </View>
+              <View style={styles.page} key="2">
+                <Text>Second page</Text>
+              </View>
+              <View style={styles.page} key="3">
+                <Text>Third page</Text>
+              </View>
+            </ViewPager>
+          </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    viewPager: {
+      flex: 1,
+    },
+    page: {
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  });
